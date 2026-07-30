ab1ba54812ad08f73633edf1e27b2b73e86525f9
gustil702 <iohann.tanferre@escola.pr.gov.br>
2026-07-30 23:11:03 +0000

    meu git

---
+++ array.js
@@ -0,0 +1,25 @@
+const alunos=['João',
+     'Juliana',
+      'Ana', 
+      'Caio', 
+      'Lara', 
+      'Marjorie',
+       'Guilherme', 
+       'Aline', 
+       'Fabiana', 
+       'Andre', 
+       'Carlos', 
+       'Paulo', 
+       'Bia', 
+       'Vivian', 
+       'Isabela', 
+       'Vinícius', 
+       'Renan', 
+       'Renata', 
+       'Daisy', 
+       'Camilo'
+    ];
+
+alunos.slice(0, 10)
+
+console.log(alunos);
